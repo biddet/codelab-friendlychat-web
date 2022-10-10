c7db0f0d476c1253d054784999ef5ecd1c673a24
biddet <96024863+biddet@users.noreply.github.com>
2022-10-09 20:02:24 -0400

    Update firebase-config.js

--- web/src/firebase-config.js
+++ web/src/firebase-config.js
@@ -1,20 +1,17 @@
-/**
- * To find your Firebase config object:
- * 
- * 1. Go to your [Project settings in the Firebase console](https://console.firebase.google.com/project/_/settings/general/)
- * 2. In the "Your apps" card, select the nickname of the app for which you need a config object.
- * 3. Select Config from the Firebase SDK snippet pane.
- * 4. Copy the config object snippet, then add it here.
- */
-const config = {
-  /* TODO: ADD YOUR FIREBASE CONFIGURATION OBJECT HERE */
-};
+ // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.11.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-export function getFirebaseConfig() {
-  if (!config || !config.apiKey) {
-    throw new Error('No Firebase configuration object provided.' + '\n' +
-    'Add your web app\'s configuration object to firebase-config.js');
-  } else {
-    return config;
-  }
-}
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyC9eVMZE0sNUjPviZnrn2Y1lUIadTWhBeY",
+    authDomain: "friend-lychat.firebaseapp.com",
+    projectId: "friend-lychat",
+    storageBucket: "friend-lychat.appspot.com",
+    messagingSenderId: "634719125304",
+    appId: "1:634719125304:web:567bdccbdaa7b5f99e8bcd"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
